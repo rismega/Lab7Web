e076924f84bfd5ebe37a2685876d86861022d8d1
Rismega <megarisamasu@gmail.com>
2022-12-10 00:11:49 +0700

    menambahkan file baru

---
+++ php_dasar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP DASAR</title>
+    </head>
+    <body>
+        <h1>Belajar PHP Dasar</h1>
+        <?php
+        echo "Hello World";
+        ?>
+   <body>
+        <h2>Menggunakan Variabel</h2>
+<?php
+$nim = "312110240";
+$nama = 'Mega Amanda Pabomaya Risamasu';
+$kelas = "TI.21.B2";
+echo "NIM : " . $nim . "<br>";
+echo "Nama : " . $nama . "<br>";
+echo "Kelas : $kelas";
+?>
+   </body>
+   </html>
